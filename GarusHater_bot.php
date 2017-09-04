ea4192b7c2247fc37a4d9645c7b37c33cc4a73f7
DenisGoncharov <goncharovst1ff@gmail.com>
2017-09-04 11:06:25 +0300

    Try to get new method

--- GarusHater_bot.php
+++ GarusHater_bot.php
@@ -13,6 +13,8 @@ $user_id  = $result["message"]["from"]["id"];
 $keyboard = [ [ "Последние статьи" ], [ "Картинка" ], [ "Гифка" ] ]; //Клавиатура
 
 if ( $text ) {
+	$reply = json_encode($result);
+	$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $reply ] );
 	if ( $text == "/start" ) {
 		$reply        = "Добро пожаловать в бота!";
 		$reply_markup = $telegram->replyKeyboardMarkup( [
@@ -35,26 +37,6 @@ if ( $text ) {
 	} elseif ( $user_id == 254346170 ) {
 		$reply     = 'Блять, заебал, не пиши сюда';
 		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $reply ] );
-	} elseif ( $text == "/help" ) {
-		$reply = "Информация с помощью.";
-		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $reply ] );
-	} elseif ( $text == "Картинка" ) {
-		$url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-		$telegram->sendPhoto( [ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ] );
-	} elseif ( $text == "Гифка" ) {
-		$url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-		$telegram->sendDocument( [ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ] );
-	} elseif ( $text == "Последние статьи" ) {
-		$html = simplexml_load_file( 'http://netology.ru/blog/rss.xml' );
-		foreach ( $html->channel->item as $item ) {
-			$reply .= "\xE2\x9E\xA1 " . $item->title . " (<a href='" . $item->link . "'>читать</a>)\n";
-		}
-		$telegram->sendMessage( [
-			'chat_id'                  => $chat_id,
-			'parse_mode'               => 'HTML',
-			'disable_web_page_preview' => true,
-			'text'                     => $reply
-		] );
 	} else {
 		$reply = "По запросу \"<b>" . $text . "</b>\" ничего не найдено.";
 		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'text' => $reply ] );
