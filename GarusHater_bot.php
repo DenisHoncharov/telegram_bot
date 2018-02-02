839f11b0f6bbf09fb7041fdb5f10bcc1419b4ae7
DenisGoncharov <goncharovst1ff@gmail.com>
2018-02-02 18:27:41 +0200

    make it better

--- GarusHater_bot.php
+++ GarusHater_bot.php
@@ -26,10 +26,10 @@ if ( $text ) {
 		} else if ( is_string( $today_events ) ) {
 			$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $today_events ] );
 		}
-	} elseif ( $text == "/debug" ) {
-		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => print_r($result) ] );
 	}
 }
+
+
 
 
 function getRandValue( $array ) {
