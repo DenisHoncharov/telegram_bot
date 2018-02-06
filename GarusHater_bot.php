63655b8042f57755982022e91e7355c3600dc80d
DenisGoncharov <goncharovst1ff@gmail.com>
2018-02-06 15:55:35 +0200

    add get function for link

--- GarusHater_bot.php
+++ GarusHater_bot.php
@@ -1,55 +1,77 @@
 <?php
-include( 'vendor/autoload.php' );	//Подключаем библиотеку
-include( 'AvailableSession.php' );	//Подключаем рассписание университета
-include( 'Link.php' ); 				//Подключаем линки для бмв
+include('vendor/autoload.php');    //Подключаем библиотеку
+include('AvailableSession.php');    //Подключаем рассписание университета
+include('Link.php');                //Подключаем линки для бмв
 
 use Telegram\Bot\Api;
 
-$telegram = new Api( '420523762:AAFPPwjtM-azuyylScQ7SYruZVta_tGC1kM' ); //Устанавливаем токен, полученный у BotFather
-$result   = $telegram->getWebhookUpdate(); //Передаем в переменную $result полную информацию о сообщении пользователя
+$telegram = new Api('420523762:AAFPPwjtM-azuyylScQ7SYruZVta_tGC1kM'); //Устанавливаем токен, полученный у BotFather
+$result = $telegram->getWebhookUpdate(); //Передаем в переменную $result полную информацию о сообщении пользователя
 
-$text    = $result["message"]["text"]; //Текст сообщения
+$text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор чата
-$name    = $result["message"]["from"]["username"]; //Юзернейм пользователя
+$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 
 $explodeText = explode(' ', $text);
 
-if ( $text ) {
-	if ( $text == "/hate" ) {
-		$garusHate = [
-			'просто привет Гарис'
-		];
-		$reply     = getRandValue( $garusHate );
-		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $reply ] );
-	} elseif ( $text == "/сегодня" ) {
+if ($text) {
+
+	$reply = '';
+	$requestParams = array(
+		'chat_id' => $chat_id,
+		'text' => '',
+		'parse_mode' => '',
+		'disable_web_page_preview' => '',
+		'disable_notification' => '',
+		'reply_to_message_id' => '',
+		'reply_markup' => ''
+	);
+
+	if ($text == "/hate") {
+		$garusHate = ['просто привет Гарис'];
+		$reply = getRandValue($garusHate);
+
+		$requestParams['text'] = $reply;
+
+	} elseif ($text == "/сегодня") {
 		$uniTable = new AvailableSession();
 		$today_events = $uniTable->getReadableTodayEvents();
-		if ( is_array( $today_events ) ) {
+		if (is_array($today_events)) {
 			$reply = $uniTable->getStringTodayEvents($today_events);
-			$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $reply ] );
-		} else if ( is_string( $today_events ) ) {
-			$telegram->sendMessage( [ 'chat_id' => $chat_id, 'text' => $today_events ] );
 		}
-	} else if ($explodeText[0] === '/bmw_e30'){
+
+		$requestParams['text'] = $reply;
+
+	} else if ($explodeText[0] === '/bmw_e30') {
 
 		$link = new Link();
 
-		if(count($explodeText) === 2){
-			$result = $link->getLinks($text);
-			$result = $link->showLinks($result);
-		} elseif (count($explodeText) === 3){
-			$result = $link->setLink($text);
+		if (count($explodeText) === 2) {
+			$reply = $link->getLinks($text);
+			$reply = $link->showLinks($reply);
+		} elseif (count($explodeText) === 3) {
+			$reply = $link->setLink($text);
+			$requestParams['disable_web_page_preview'] = true;
+			$requestParams['parse_mode'] = 'HTML';
 		}
 
-		$telegram->sendMessage( [ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $result ] );
+		$requestParams['text'] = $reply;
 	}
+
+	error_log('Response:');
+	error_log(print_r($result));
+
+	error_log('Request:');
+	error_log($requestParams['text']);
+
+	$telegram->sendMessage($requestParams);
+
 }
 
 
-
-function getRandValue( $array ) {
-	$lenth     = count( $array );
-	$randValue = $array[ rand( 0, $lenth ) ];
+function getRandValue($array) {
+	$lenth = count($array);
+	$randValue = $array[rand(0, $lenth)];
 
 	return $randValue;
 }
